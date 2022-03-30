5bbf339b86342b5c73443a5d06dc6ddbfac27cd2
jydoskey <jideomotola33@gmail.com>
2022-03-31 00:31:57 +0100

    imported header to homepage file

---
+++ src/components/HomePage.vue
@@ -0,0 +1,31 @@
+<template>
+    <section>
+        <Header></Header>
+    </section>
+</template>
+
+<script>
+import Header from "@/components/layout/Header.vue";
+export default {
+    name: 'HeyJidePortfolioHomepage',
+    data() {
+        return {
+            
+        };
+    },
+    components: {
+        Header
+    },
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style>
+
+</style>
