ae5666ce8b432f6a5f59c4d49e27bddc0b674cf5
jydoskey <jideomotola33@gmail.com>
2022-03-31 04:14:17 +0100

    added over effect to projects

--- src/components/layout/Projects.vue
+++ src/components/layout/Projects.vue
@@ -1,34 +1,117 @@
 <template>
   <div class="container mt-16 mx-auto">
-     <div class="container mx-auto">
-    <div class="grid-cols-3 p-20 space-y-2 bg-blue-200 lg:space-y-0 lg:grid lg:gap-3 lg:grid-rows-3">
-        <div class="w-full rounded hover:shadow-2xl cursor-pointer">
-            <img class="h-full w-full" src="src/assets/Chair-1.PNG"
-                alt="Chair-image" title="Reading Chair">
+    <div class="container mx-auto">
+      <div
+        class="
+          grid-cols-3
+          p-20
+          space-y-2
+          bg-blue-200
+          lg:space-y-0 lg:grid lg:gap-3 lg:grid-rows-3
+        "
+      >
+        <div class="w-full rounded hover:shadow-2xl cursor-pointer figure">
+          <img
+            class="image-main h-full w-full"
+            src="src/assets/Chair-1.PNG"
+            srcset="src/assets/Chair-1.PNG"
+            alt="Chair-image 1"
+            title="Reading Chair"
+          />
+          <img
+            class="image-hover h-full w-full"
+            src="src/assets/Chair-2.PNG"
+            srcset="src/assets/Chair-2.PNG"
+            alt="Chair-image 2"
+            title="Reading Chair"
+          />
         </div>
-        <div class="w-full col-span-2 row-span-2 rounded hover:shadow-2xl cursor-pointer">
-            <img class="h-full w-full" src="src/assets/Helmet-1.PNG"
-                alt="Helmet-image" title="Helmet">
+
+        <div
+          class="
+            w-full
+            col-span-2
+            row-span-2
+            rounded
+            hover:shadow-2xl
+            cursor-pointer
+            figure
+          "
+        >
+          <img
+            class="image-main h-full w-full"
+            src="src/assets/Helmet-1.PNG"
+            srcset="src/assets/Helmet-1.PNG"
+            alt="Helmet-image 1"
+            title="Helmet"
+          />
+          <img
+            class="image-hover h-full w-full"
+            src="src/assets/Helmet-2.PNG"
+            srcset="src/assets/Helmet-2.PNG"
+            alt="Helmet-image 2"
+            title="Helmet"
+          />
         </div>
-        <div class="w-full rounded hover:shadow-2xl cursor-pointer">
-            <img class="h-full w-full" src="src/assets/Bookshelf-1.PNG"
-                alt="Bookshelf-image" title="Bookshelf">
+        
+        <div class="w-full rounded hover:shadow-2xl cursor-pointer figure">
+          <img
+            class="image-main h-full w-full"
+            src="src/assets/Bookshelf-1.PNG"
+            srcset="src/assets/Bookshelf-1.PNG"
+            alt="Bookshelf-image 1"
+            title="Bookshelf"
+          />
+          <img
+            class="image-hover h-full w-full"
+            src="src/assets/Bookshelf-2.PNG"
+            srcset="src/assets/Bookshelf-2.PNG"
+            alt="Bookshelf-image 2"
+            title="Bookshelf"
+          />
         </div>
-        <div class="w-full rounded hover:shadow-2xl cursor-pointer">
-            <img class="h-full w-full" src="src/assets/Bus-shelters-1.PNG"
-                alt="Bus-Shelters-image" title="Bus Shelters">
+
+        <div class="w-full rounded hover:shadow-2xl cursor-pointer figure">
+          <img
+            class="image-main h-full w-full"
+            src="src/assets/Bus-shelters-1.PNG"
+            srcset="src/assets/Bus-shelters-1.PNG"
+            alt="Bus-Shelters-image 1"
+            title="Bus Shelters"
+          />
+          <img
+            class="image-hover h-full w-full"
+            src="src/assets/Bus-shelters-2.PNG"
+            srcset="src/assets/Bus-shelters-2.PNG"
+            alt="Bus-Shelters-image 2"
+            title="Bus Shelters"
+          />
         </div>
-        <div class="w-full rounded hover:shadow-2xl cursor-pointer">
-            <img class="h-full w-full" src="src/assets/Fusion-1.PNG"
-                alt="Fusion-image" title="Fusion">
+
+        <div class="w-full rounded hover:shadow-2xl cursor-pointer figure">
+          <img
+            class="image-main h-full w-full"
+            src="src/assets/Fusion-1.PNG"
+            srcset="src/assets/Fusion-1.PNG"
+            alt="Fusion-image 1"
+            title="Fusion"
+          />
+          <img
+            class="image-hover h-full w-full"
+            src="src/assets/Fusion-2.PNG"
+            srcset="src/assets/Fusion-2.PNG"
+            alt="Fusion-image 2"
+            title="Fusion"
+          />
         </div>
+
         <!-- <div class="w-full rounded hover:shadow-2xl cursor-pointer">
             <img class="h-full w-full" src=""
                 alt="image">
         </div> -->
+      </div>
     </div>
-</div>
-    </div>
+  </div>
 </template>
 
 <script>
@@ -39,6 +122,8 @@ export default {
     return {};
   },
 
+  computed: {},
+
   mounted() {},
 
   methods: {},
@@ -46,4 +131,23 @@ export default {
 </script>
 
 <style>
+/*
+      Rollover Image
+     */
+.figure {
+  position: relative;
+}
+.figure img.image-hover {
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  object-fit: cover;
+  opacity: 0;
+  transition: opacity 0.2s;
+}
+.figure:hover img.image-hover {
+  opacity: 1;
+}
 </style>
