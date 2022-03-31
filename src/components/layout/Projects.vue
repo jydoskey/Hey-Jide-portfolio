253cff4567bd9d98ad11902f5fa2f2a69f2d7339
jydoskey <jideomotola33@gmail.com>
2022-03-31 03:34:50 +0100

    added project section to homepage

---
+++ src/components/layout/Projects.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container mt-16 mx-auto">
+     <div class="container mx-auto">
+    <div class="grid-cols-3 p-20 space-y-2 bg-blue-200 lg:space-y-0 lg:grid lg:gap-3 lg:grid-rows-3">
+        <div class="w-full rounded hover:shadow-2xl cursor-pointer">
+            <img class="h-full w-full" src="src/assets/Chair-1.PNG"
+                alt="Chair-image" title="Reading Chair">
+        </div>
+        <div class="w-full col-span-2 row-span-2 rounded hover:shadow-2xl cursor-pointer">
+            <img class="h-full w-full" src="src/assets/Helmet-1.PNG"
+                alt="Helmet-image" title="Helmet">
+        </div>
+        <div class="w-full rounded hover:shadow-2xl cursor-pointer">
+            <img class="h-full w-full" src="src/assets/Bookshelf-1.PNG"
+                alt="Bookshelf-image" title="Bookshelf">
+        </div>
+        <div class="w-full rounded hover:shadow-2xl cursor-pointer">
+            <img class="h-full w-full" src="src/assets/Bus-shelters-1.PNG"
+                alt="Bus-Shelters-image" title="Bus Shelters">
+        </div>
+        <div class="w-full rounded hover:shadow-2xl cursor-pointer">
+            <img class="h-full w-full" src="src/assets/Fusion-1.PNG"
+                alt="Fusion-image" title="Fusion">
+        </div>
+        <!-- <div class="w-full rounded hover:shadow-2xl cursor-pointer">
+            <img class="h-full w-full" src=""
+                alt="image">
+        </div> -->
+    </div>
+</div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Projects",
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style>
+</style>
