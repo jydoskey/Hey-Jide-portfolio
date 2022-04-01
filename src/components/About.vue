fe6692bff225844095c77bd5439a8c14397d45c3
jydoskey <jideomotola33@gmail.com>
2022-04-01 02:00:15 +0100

    about page skills, education, contact etc. added

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <Header></Header>
+
     <div class="container mt-10 mx-auto">
       <div class="container mx-auto">
         <div
@@ -12,6 +13,19 @@
             lg:space-y-0 lg:grid lg:gap-2 lg:grid-rows-1 lg:mx-auto
           "
         >
+          <img
+            class="
+              bg-gray-200
+              object-fit
+              w-72
+              lg:justify-self-end
+              md:mx-auto
+              my-5
+              lg:my-0
+            "
+            alt="Jumoke"
+            src="@/assets/images/projet-Images/jumoke.png"
+          />
           <div class="self-center">
             <h1 class="text-base text-slate-400 mb-4 dark:text-white">
               Hello, I'm Olajumoke
@@ -28,22 +42,75 @@
               hope to do this is through empathy in design and innovation
             </p>
           </div>
-          <img
-            class="
-              bg-gray-200
-              object-fit
-              w-72
-              lg:justify-self-end
-              md:mx-auto
-              my-5
-              lg:my-0
-            "
-            alt="Jumoke"
-            src="@/assets/images/projet-Images/jumoke.png"
-          />
         </div>
       </div>
     </div>
+
+    <div class="container mt-10 mx-auto">
+      <div class="container mx-auto">
+        <div
+          class="
+            grid-cols-2
+            py-10
+            lg:px-20
+            md:px-10
+            lg:space-y-0 lg:grid lg:gap-2 lg:grid-rows-1 lg:mx-auto
+          "
+        >
+          <div>
+            <h1 class="text-base text-slate-400 mb-2 dark:text-white">
+              EDUCATION
+            </h1>
+            <p
+              class="text-base font-semibold text-gray-800 mb-6 dark:text-white"
+            >
+              Obafemi Awolowo University, 2014 - 2019
+              <span class="block"
+                >• BSC, Electrical and Electronic Engineering</span
+              >
+            </p>
+            <h1 class="text-base text-slate-400 mb-2 dark:text-white">
+              SOLD?, LET'S CONNECT
+            </h1>
+            <p class="text-base text-gray-800 mb-4 dark:text-white">
+              Say hello at olajumokeademodi@gmail.com
+            </p>
+            <h1 class="text-base text-slate-400 mb-2 dark:text-white">
+              LANGUAGE
+            </h1>
+            <p class="text-base text-gray-800 mb-4 dark:text-white">
+              English and French
+            </p>
+          </div>
+
+          <div>
+            <h1 class="text-base text-slate-400 mb-2 dark:text-white">
+              SKILLS
+            </h1>
+            <p
+              class="text-base font-semibold text-gray-800 mb-6 dark:text-white"
+            >
+              SOFTWARE / TOOL
+              <span class="block"
+                >Whimsical, Jira, Figma, Solidworks, Balsamiq, Intel
+                Galileo</span
+              >
+            </p>
+            <h1 class="text-base text-slate-400 mb-2 dark:text-white">
+              SPORTS
+            </h1>
+            <p class="text-base text-gray-800 mb-4 dark:text-white">
+              Lawn Tennis, Table Tennis
+            </p>
+            <h1 class="text-base text-slate-400 mb-2 dark:text-white">ART</h1>
+            <p class="text-base text-gray-800 mb-4 dark:text-white">
+              Painting, Drawing
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <Footer></Footer>
   </div>
 </template>
